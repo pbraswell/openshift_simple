5bbb0d8a5a235bdc1eacb6b20c5e785e4ab48a07
Peter Braswell <peter.braswell@gmail.com>
2012-12-12 11:38:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenshiftSimpleGit::Application.config.secret_token = '18be1d6a6a595fb51984d6aa794255d52812c8918b8eec115286226a9c86b4d663c17485356eb2e27d71c2006eb13b21c4018194c27c003d1ce73d6a46114eb1'
